e01a83a553605df0138f5e1a9f9703f544a5fb0c
Chris Conover <clcono@gmail.com>
2012-10-17 09:54:03 -0400

    Remove social links

--- header.php
+++ header.php
@@ -54,17 +54,7 @@
 			<div class="row">
 				<div id="header" class="span12">
 					<h1><a href="<?=bloginfo('url')?>"><?=bloginfo('name')?></a></h1>
-					<?php $options = get_option(THEME_OPTIONS_NAME);?>
-					<?php if($options['facebook_url'] or $options['twitter_url']):?>
-					<ul class="social menu horizontal span3">
-						<?php if($options['facebook_url']):?>
-						<li><a class="ignore-external facebook" href="<?=$options['facebook_url']?>">Facebook</a></li>
-						<?php endif;?>
-						<?php if($options['twitter_url']):?>
-						<li><a class="ignore-external twitter" href="<?=$options['twitter_url']?>">Twitter</a></li>
-						<?php endif;?>
-					</ul>
-					<?php endif;?>
+					
 				</div>
 			</div>
 			<?=wp_nav_menu(array(
