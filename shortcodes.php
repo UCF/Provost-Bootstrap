f6a1a2b829679662258f7fa3c0b88a0d2ae5f843
Chris Conover <clcono@gmail.com>
2012-10-17 15:28:13 -0400

    Added sc_org_chart shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -395,4 +395,101 @@ function sc_faculty_award_programs($attrs) {
 	return ob_get_clean();
 }
 add_shortcode('sc-faculty-award-programs', 'sc_faculty_award_programs');
+
+function sc_org_chart($attrs) {
+	$deans_list = get_posts(array(
+		'numberposts' => 1,
+		'post_type'   => 'provost_form',
+		'category'    => get_category_by_slug('deans-list')->term_id,
+	));
+	if (count($deans_list)){
+		$deans_list = $deans_list[0];
+	}
+	$org_chart = get_posts(array(
+		'numberposts' => 1,
+		'post_type'   => 'provost_form',
+		'category'    => get_category_by_slug('org-chart')->term_id,
+	));
+	if (count($org_chart)){
+		$org_chart = $org_chart[0];
+	}
+	
+	
+	$category = get_category_by_slug('college-deans');
+	$college_deans = ($category) ? get_posts(array(
+		'numberposts' => -1,
+		'post_type'   => 'profile',
+		'category'    => $category->term_id,
+		'orderby'     => 'menu_order',
+		'order'       => 'ASC',
+	)) : false;
+	
+	$category = get_category_by_slug('academic-officers');
+	$academic_officers = ($category) ? get_posts(array(
+		'numberposts' => -1,
+		'post_type'   => 'profile',
+		'category'    => $category->term_id,
+		'orderby'     => 'menu_order',
+		'order'       => 'ASC',
+	)) : false;
+	
+	$category = get_category_by_slug('administrative-staff');
+	$administrative_staff = ($category) ? get_posts(array(
+		'numberposts' => -1,
+		'post_type'   => 'profile',
+		'category'    => get_category_by_slug('administrative-staff')->term_id,
+		'orderby'     => 'menu_order',
+		'order'       => 'ASC',
+	)) : false;
+	
+	function display_people($people, $id=null){
+		?>
+		<div class="row">
+		<?
+		$count = 0;
+		foreach($people as $person) {
+			if($count > 0 && ($count % 4) == 0) {
+				echo '</div><div class="row">';
+			}
+			?>
+			<div class="span2">
+			<a href="<?=get_permalink($person->ID)?>">
+			<?php
+				$img = get_the_post_thumbnail($person->ID);
+				if ($img):?>
+				<?=$img?>
+				<?php else:?>
+					<img src="<?=PROVOST_IMG_URL?>/no-photo.png" alt="Photo Unavailable" />
+				<?php endif;?>
+				<span class="name"><?=str_replace('', '&nbsp;', $person->post_title)?></span>
+			</a>
+			<span class="description"><?=get_post_meta($person->ID, 'profile_description', True)?></span>	
+			</div>
+			<?
+			$count++;
+		}
+		?> </div> <?
+	}
+	ob_start();
+	?>
+	<div id="org-chart">
+		<?php if ($academic_officers):?>
+		<h3><?=get_category_by_slug('academic-officers')->name ?> <small><a href="<?=Document::get_url($org_chart)?>">Download PDF <?=$org_chart->post_title?></a></small></h3>
+		<?php display_people($academic_officers, 'academic-officers');?>
+		<?php endif;?>
+		
+		<?php if ($college_deans):?>
+		<h3><?=get_category_by_slug('college-deans')->name ?> <small><a href="<?=Document::get_url($deans_list)?>">Download PDF <?=$deans_list->post_title?></a></small></h3>
+		<?php display_people($college_deans, 'college-deans');?>
+		<?php endif;?>
+		
+		<?php if ($administrative_staff):?>
+		<h3><?=get_category_by_slug('administrative-staff')->name ?></h3>
+		<?php display_people($administrative_staff, 'administrative-staff');?>
+		<?php endif;?>
+	</div>
+	<?
+	return ob_get_clean();
+}
+add_shortcode('sc-org-chart', 'sc_org_chart');
 ?>
