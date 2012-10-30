5c36458981a376eb15df63feaf238259e610e834
Chris Conover <clcono@gmail.com>
2012-10-30 14:53:52 -0400

    Added custom post types

--- custom-post-types.php
+++ custom-post-types.php
@@ -243,4 +243,363 @@ abstract class CustomPostType{
 	}
 }
 
+class Document extends CustomPostType{
+	public
+		$name           = 'provost_form',
+		$plural_name    = 'Documents',
+		$singular_name  = 'Document',
+		$add_new_item   = 'Add New Document',
+		$edit_item      = 'Edit Document',
+		$new_item       = 'New Document',
+		$use_title      = True,
+		$use_editor     = False,
+		$use_shortcode  = True,
+		$use_metabox    = True;
+	
+	public function fields(){
+		$fields   = parent::fields();
+		$fields[] = array(
+			'name' => __('URL'),
+			'desc' => __('Associate this document with a URL.  This will take precedence over any uploaded file, so leave empty if you want to use a file instead.'),
+			'id'   => $this->options('name').'_url',
+			'type' => 'text',
+		);
+		$fields[] = array(
+			'name'    => __('File'),
+			'desc'    => __('Associate this document with an already existing file.'),
+			'id'      => $this->options('name').'_file',
+			'type'    => 'file',
+		);
+		return $fields;
+	}
+	
+	
+	static function get_document_application($form){
+		return mimetype_to_application(self::get_mimetype($form));
+	}
+	
+	
+	static function get_mimetype($form){
+		if (is_numeric($form)){
+			$form = get_post($form);
+		}
+		
+		$prefix   = post_type($form);
+		$document = get_post(get_post_meta($form->ID, $prefix.'_file', True));
+		
+		$is_url = get_post_meta($form->ID, $prefix.'_url', True);
+		
+		return ($is_url) ? "text/html" : $document->post_mime_type;
+	}
+	
+	
+	static function get_title($form){
+		if (is_numeric($form)){
+			$form = get_post($form);
+		}
+		
+		$prefix = post_type($form);
+		
+		return $form->post_title;
+	}
+	
+	static function get_url($form){
+		if (is_numeric($form)){
+			$form = get_post($form);
+		}
+		
+		$prefix = post_type($form);
+		
+		$x = get_post_meta($form->ID, $prefix.'_url', True);
+		$y = wp_get_attachment_url(get_post_meta($form->ID, $prefix.'_file', True));
+		
+		if (!$x and !$y){
+			return '#';
+		}
+		
+		return ($x) ? $x : $y;
+	}
+	
+	
+	/**
+	 * Handles output for a list of objects, can be overridden for descendants.
+	 * If you want to override how a list of objects are outputted, override
+	 * this, if you just want to override how a single object is outputted, see
+	 * the toHTML method.
+	 **/
+	public function objectsToHTML($objects, $css_classes){
+		if (count($objects) < 1){ return '';}
+		
+		$class_name = get_custom_post_type($objects[0]->post_type);
+		$class      = new $class_name;
+		
+		ob_start();
+		?>
+		<ul class="nobullet <?php if($css_classes):?><?=$css_classes?><?php else:?><?=$class->options('name')?>-list<?php endif;?>">
+			<?php foreach($objects as $o):?>
+			<li class="document <?=$class_name::get_document_application($o)?>">
+				<?=$class->toHTML($o)?>
+			</li>
+			<?php endforeach;?>
+		</ul>
+		<?php
+		$html = ob_get_clean();
+		return $html;
+	}
+	
+	
+	/**
+	 * Outputs this item in HTML.  Can be overridden for descendants.
+	 **/
+	public function toHTML($object){
+		$title = Document::get_title($object);
+		$url   = Document::get_url($object);
+		$html = "<a href='{$url}'>{$title}</a>";
+		return $html;
+	}
+}
+
+class Page extends CustomPostType {
+	public
+		$name           = 'page',
+		$plural_name    = 'Pages',
+		$singular_name  = 'Page',
+		$add_new_item   = 'Add New Page',
+		$edit_item      = 'Edit Page',
+		$new_item       = 'New Page',
+		$public         = True,
+		$use_editor     = True,
+		$use_thumbnails = False,
+		$use_order      = True,
+		$use_title      = True,
+		$use_metabox    = True,
+		$built_in       = True;
+
+	public function fields() {
+		$prefix = $this->options('name').'_';
+		return array(
+			array(
+				'name' => 'Hide Lower Section',
+				'desc' => 'This section normally contains the Flickr, News and Events widgets. The footer will not be hidden',
+				'id'   => $prefix.'hide_fold',
+				'type' => 'checkbox',
+			),
+				array(
+					'name' => 'Stylesheet',
+					'desc' => '',
+					'id' => $prefix.'stylesheet',
+					'type' => 'file',
+				),
+		);
+	}
+}
+
+/**
+ * Describes a staff member
+ *
+ * @author Chris Conover
+ **/
+class Person extends CustomPostType
+{
+	public
+		$name           = 'profile',
+		$plural_name    = 'People',
+		$singular_name  = 'Person',
+		$add_new_item   = 'Add Person',
+		$edit_item      = 'Edit Person',
+		$new_item       = 'New Person',
+		$public         = True,
+		$use_shortcode  = True,
+		$use_metabox    = True,
+		$use_thumbnails = True,
+		$use_order      = True,
+		$taxonomies     = array('org_groups', 'category');
+
+		public function fields(){
+			$fields = array(
+				array(
+					'name'    => 'Description',
+					'desc'    => 'Position, title, etc.',
+					'id'      => $this->options('name').'_description',
+					'type'    => 'text',
+				),
+			);
+			return $fields;
+		}
+
+	public function get_objects($options=array()){
+		$options['order']    = 'ASC';
+		$options['orderby']  = 'person_orderby_name';
+		$options['meta_key'] = 'person_orderby_name';
+		return parent::get_objects($options);
+	}
+
+	public static function get_name($person) {
+		$prefix = get_post_meta($person->ID, 'person_title_prefix', True);
+		$suffix = get_post_meta($person->ID, 'person_title_suffix', True);
+		$name = $person->post_title;
+		return $prefix.' '.$name.' '.$suffix;
+	}
+
+	public static function get_phones($person) {
+		$phones = get_post_meta($person->ID, 'person_phones', True);
+		return ($phones != '') ? explode(',', $phones) : array();
+	}
+
+	public function objectsToHTML($people, $css_classes) {
+		ob_start();?>
+		<div class="row">
+			<div class="span12">
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th scope="col" class="name">Name</th>
+							<th scope="col" class="job_title">Title</th>
+							<th scope="col" class="phones">Phone</th>
+							<th scope="col" class="email">Email</th>
+						</tr>
+					</thead>
+					<tbody>
+				<?
+				foreach($people as $person) { 
+					$email = get_post_meta($person->ID, 'person_email', True); 
+					$link = ($person->post_content == '') ? False : True; ?>
+						<tr>
+							<td class="name">
+								<?if($link) {?><a href="<?=get_permalink($person->ID)?>"><?}?>
+									<?=$this->get_name($person)?>
+								<?if($link) {?></a><?}?>
+							</td>
+							<td class="job_title">
+								<?if($link) {?><a href="<?=get_permalink($person->ID)?>"><?}?>
+								<?=get_post_meta($person->ID, 'person_jobtitle', True)?>
+								<?if($link) {?></a><?}?>
+							</td> 
+							<td class="phones"><?php if(($link) && ($this->get_phones($person))) {?><a href="<?=get_permalink($person->ID)?>">
+								<?php } if($this->get_phones($person)) {?>
+									<ul class="unstyled"><?php foreach($this->get_phones($person) as $phone) { ?><li><?=$phone?></li><?php } ?></ul>
+								<?php } if(($link) && ($this->get_phones($person))) {?></a><?php }?></td>
+							<td class="email"><?=(($email != '') ? '<a href="mailto:'.$email.'">'.$email.'</a>' : '')?></td>
+						</tr>
+				<? } ?>
+				</tbody>
+			</table> 
+		</div>
+	</div><?
+	return ob_get_clean();
+	}
+} // END class 
+
+class Post extends CustomPostType {
+	public
+		$name           = 'post',
+		$plural_name    = 'Posts',
+		$singular_name  = 'Post',
+		$add_new_item   = 'Add New Post',
+		$edit_item      = 'Edit Post',
+		$new_item       = 'New Post',
+		$public         = True,
+		$use_editor     = True,
+		$use_thumbnails = False,
+		$use_order      = True,
+		$use_title      = True,
+		$use_metabox    = True,
+		$taxonomies     = array('post_tag', 'category'),
+		$built_in       = True;
+
+	public function fields() {
+		$prefix = $this->options('name').'_';
+		return array(
+			array(
+				'name' => 'Hide Lower Section',
+				'desc' => 'This section normally contains the Flickr, News and Events widgets. The footer will not be hidden',
+				'id'   => $prefix.'hide_fold',
+				'type' => 'checkbox',
+			),
+				array(
+					'name' => 'Stylesheet',
+					'desc' => '',
+					'id' => $prefix.'stylesheet',
+					'type' => 'file',
+				),
+		);
+	}
+}
+
+class Help extends CustomPostType {
+	public
+		$name           = 'provost_help',
+		$plural_name    = 'Help',
+		$singular_name  = 'Help',
+		$add_new_item   = 'Add New Help',
+		$edit_item      = 'Edit Help',
+		$new_item       = 'New Help',
+		$use_metabox    = True,
+		$use_editor     = False,
+		$use_thumbnails = False,
+		$use_order      = False,
+		$taxonomies     = array();
+
+
+	public function fields() {
+		$id_prefix  = $this->options('name');
+		$documents  = new Document();
+		return array(
+			array(
+				'name' => 'url',
+				'desc' => 'URL',
+				'id'   => $id_prefix.'_url',
+				'type' => 'text',
+			),
+			array(
+				'name'    => 'forms',
+				'desc'    => 'You can define a url or select an existing form.',
+				'id'      => $id_prefix.'_forms',
+				'type'    => 'select',
+				'options' => $documents->get_objects_as_options()
+			)
+		);
+	}
+}
+
+class Update extends CustomPostType {
+	public
+		$name           = 'provost_update',
+		$plural_name    = 'Updates',
+		$singular_name  = 'Update',
+		$add_new_item   = 'Add New Update',
+		$edit_item      = 'Edit Update',
+		$new_item       = 'New Update';
+}
+
+class HomeImage extends CustomPostType {
+	public
+		$name           = 'provost_home_images',
+		$plural_name    = 'Home Images',
+		$singular_name  = 'Home Imge',
+		$add_new_item   = 'Add New Home Image',
+		$edit_item      = 'Edit Home Image',
+		$new_item       = 'New Home Image';
+}
+
+class Unit extends CustomPostType {
+	public
+		$name           = 'provost_unit',
+		$plural_name    = 'Colleges/Units',
+		$singular_name  = 'College/Unit',
+		$add_new_item   = 'Add New College/Unit',
+		$edit_item      = 'Edit College/Unit',
+		$new_item       = 'New College/Unit';
+}
+
+class AwardProgram extends CustomPostType {
+	public
+		$name           = 'provost_award',
+		$plural_name    = 'Award Programs',
+		$singular_name  = 'Award Program',
+		$add_new_item   = 'Add New Award Program',
+		$edit_item      = 'Edit Award Program',
+		$new_item       = 'New Award Program';
+}
+
 ?>
