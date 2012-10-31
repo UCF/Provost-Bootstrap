90b7730a4b2648b6910a25aef4cbca3908c08498
Chris Conover <clcono@gmail.com>
2012-10-31 12:06:27 -0400

    Factuly excellence page

---
+++ page-faculty-excellence-awards.php
@@ -0,0 +1,8 @@
+<?php get_header(); the_post();?>
+<div class="row" id="<?=$post->post_name?>">
+	<div class="span12 page-content">
+		<h2 class="page-header"><?php the_title();?></h2>
+		<?php the_content();?>
+	</div>
+</div>
+<?php get_footer();?>
