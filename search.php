8090914de91a7bdb1deff42d38fbe445feb2e47f
Chris Conover <clcono@gmail.com>
2012-10-31 15:17:57 -0400

    Search site domain instead of ucf.edu

--- search.php
+++ search.php
@@ -4,7 +4,7 @@
 	$domain  = $options['search_domain'];
 	$limit   = (int)$options['search_per_page'];
 	$start   = (is_numeric($_GET['start'])) ? (int)$_GET['start'] : 0;
-	$results = get_search_results($_GET['s'], $start, $limit, 'www.ucf.edu');
+	$results = get_search_results($_GET['s'], $start, $limit, $domain);
 ?>
 <?php get_header(); ?>
 	<div class="row page-content" id="search-results">
