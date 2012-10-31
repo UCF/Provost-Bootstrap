1fd949d09dd66be6badd8a69342630bb30ca568e
Chris Conover <clcono@gmail.com>
2012-10-31 11:38:24 -0400

    Boostrap search form

---
+++ searchform.php
@@ -0,0 +1,6 @@
+
+<form class="form-search" role="search" method="get" action="<?=home_url( '/' )?>">
+	<label for="s">Search:</label><br />
+	<input type="text" class="input-large search-query" value="<?=htmlentities($_GET['s'])?>" name="s" placeholder="Enter your search term here...">
+	<button type="submit" class="btn">Search</button>
+</form>
