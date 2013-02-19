a4e778ca928f45a30b0b76511d4027385a7d3301
Jo Greybill <jogreybill@gmail.com>
2013-02-19 11:28:35 -0500

    Re-added webcom-base.js stuff for Analytics, etc

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,38 @@
-$().ready(function() {
-	
-	$('.select-links')
-		.change(function() {
-			var selected = $(this).children('option:selected');
-			if(typeof selected != 'undefined') {
-				var url = selected.val();
-				if(url != '') {
-					window.location = url;
-				}
-			}
-		});
+if (typeof jQuery != 'undefined'){
+	jQuery(document).ready(function($) {
+		Webcom.slideshow($);
+		Webcom.analytics($);
+		Webcom.handleExternalLinks($);
+		Webcom.loadMoreSearchResults($);
 		
-	$('#home-images-carousel').carousel('cycle');
-	
-	// menu nav separators
-	$('.menu li:last-child').addClass('last');
-	if ($.browser.msie && $.browser.version < 9) {
-		$('.menu li').append('<span class="ieseparator">•</span>');
-		$('.menu li.last .ieseparator').remove();
-	}
-	
-	// tons of tables? why not javascript? (\/) (°,,,°) (\/)
-	$('.page-content table.blue').addClass('table table-striped');
-	
-	// ie fix for stripey tables
-	if ($.browser.msie && $.browser.version < 9) {
-		$('.table-striped tbody tr:nth-child(even) td, .table-striped tbody tr:nth-child(even) th').css('background-color','#E5ECF9');
-		$('#faculty-excellence-awards .table-striped tbody tr:nth-child(even) td, #faculty-excellence-awards.table-striped tbody tr:nth-child(even) th').css('background-color','#fff');
-		$('#faculty-excellence-awards .table-striped tbody tr:nth-child(odd) td, #faculty-excellence-awards.table-striped tbody tr:nth-child(odd) th').css('background-color','#E5ECF9');
-	}
-});
+		$('.select-links')
+			.change(function() {
+				var selected = $(this).children('option:selected');
+				if(typeof selected != 'undefined') {
+					var url = selected.val();
+					if(url != '') {
+						window.location = url;
+					}
+				}
+			});
+			
+		$('#home-images-carousel').carousel('cycle');
+		
+		// menu nav separators
+		$('.menu li:last-child').addClass('last');
+		if ($.browser.msie && $.browser.version < 9) {
+			$('.menu li').append('<span class="ieseparator">•</span>');
+			$('.menu li.last .ieseparator').remove();
+		}
+		
+		// tons of tables? why not javascript? (\/) (°,,,°) (\/)
+		$('.page-content table.blue').addClass('table table-striped');
+		
+		// ie fix for stripey tables
+		if ($.browser.msie && $.browser.version < 9) {
+			$('.table-striped tbody tr:nth-child(even) td, .table-striped tbody tr:nth-child(even) th').css('background-color','#E5ECF9');
+			$('#faculty-excellence-awards .table-striped tbody tr:nth-child(even) td, #faculty-excellence-awards.table-striped tbody tr:nth-child(even) th').css('background-color','#fff');
+			$('#faculty-excellence-awards .table-striped tbody tr:nth-child(odd) td, #faculty-excellence-awards.table-striped tbody tr:nth-child(odd) th').css('background-color','#E5ECF9');
+		}
+	});
+}else{console.log('jQuery dependancy failed to load');}
