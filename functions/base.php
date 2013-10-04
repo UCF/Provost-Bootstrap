bbfed5f1d3c665f8d1f8827e5fd3fd18a83b4d34
Jo Greybill <jogreybill@gmail.com>
2013-10-04 12:33:24 -0400

    Add ID param to ucf header script when registered with wp_enqueue_script

--- functions/base.php
+++ functions/base.php
@@ -450,6 +450,19 @@ function indent($html, $n){
  ***************************************************************************/
 
 /**
+ * University Header enqueue-ing fix.
+ **/
+function add_id_to_ucfhb($url) {
+    if ( (false !== strpos($url, 'bar/js/university-header.js')) || (false !== strpos($url, 'bar/js/university-header-full.js')) ) {
+      remove_filter('clean_url', 'add_id_to_ucfhb', 10, 3);
+      return "$url' id='ucfhb-script";
+    }
+    return $url;
+}
+add_filter('clean_url', 'add_id_to_ucfhb', 10, 3);
+
+
+/**
  * Walker function to add Bootstrap classes to nav menus using wp_nav_menu()
  * 
  * based on https://gist.github.com/1597994
