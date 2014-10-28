a6efa3c07a8fa96e3ea43f500699c2f50636974d
Brandon Groves <brandon.groves@gmail.com>
2014-10-28 14:38:04 -0400

    modified the featured image generation to match the new plugin style.

--- functions/config.php
+++ functions/config.php
@@ -31,8 +31,8 @@ add_action('after_setup_theme', '__init__');
  * Remove height and width
  */
 function remove_thumbnail_dimensions( $html, $post_id, $post_image_id ) {
-	$post_template = get_post_meta( $post_id, 'custom_post_template', true );
-	if ($post_template ==='dale.php') {
+	$post_template = get_post_meta( $post_id, '_post_template', true );
+	if ($post_template === 'provost-update-dale.php') {
 		$html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
 		$html = preg_replace( '/ wp-post-image/', "", $html );
 	}
