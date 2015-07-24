791a8ed94994c26fc06d20609afa8c60c33dd029
Jo Dickson <jogreybill@gmail.com>
2015-07-24 09:22:05 -0500

    Re-added provost update templates and static assets

---
+++ provost-update-diane.php
@@ -0,0 +1,8 @@
+<?php
+/*
+WP Post Template: Diane Chase
+Description: Requires WP Post Template plugin.
+*/
+the_post();
+$signature = 'chase';
+require_once 'provost-update-signature.php' ;
---
+++ provost-update-terry.php
@@ -0,0 +1,8 @@
+<?php
+/*
+WP Post Template: Terry Hickey
+Description: Requires WP Post Template plugin.
+*/
+the_post();
+$signature = 'terry';
+require_once 'provost-update-signature.php';
