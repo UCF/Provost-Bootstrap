4bf08edca001cdc46b06ed674b0e030eb3fc38cc
Jim Barnes <james.barnes@ucf.edu>
2015-08-12 12:30:52 -0400

    Added missing closing h2.

--- template-json-only.php
+++ template-json-only.php
@@ -12,7 +12,7 @@ if ( $post ):
 
 	<div class="row" id="<?php echo $post->post_name; ?>">
 		<div class="span-10">
-			<h2><?php echo the_title(); ?>
+			<h2><?php echo the_title(); ?></h2>
 			<?php echo the_content(); ?>
 		</div>
 	</div>
